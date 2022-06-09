2ac20dadca331816e27aeb463c1db6ca5d1d5ea8
Yoonmin <jeongym.sw@gmail.com>
2022-06-09 13:11:16 +0900

    word2 자음확인

---
+++ j/word2.java
@@ -0,0 +1,24 @@
+package j;
+
+public class word2 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		word word = new word("hello world");
+		System.out.println(word.isVowel(0));
+
+	}
+	private String letters;
+	private int result;
+	public word(String letters) {
+		this.letters = letters.toLowerCase();
+	}
+	
+	//i번째 글자가 모음인지
+	public boolean isVowel(int i) {
+		return "aueio".contains(letters.substring(i, i + 1));
+	}
+	//i번째 글자가 자음인지
+	public boolean isConsonant(int i) {
+		return !("aueio".contains(letters.substring(i, i + 1)));
+	}
+}
